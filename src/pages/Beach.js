1d2004ceae20a3f7e4c499a9e6b022b58cbbe31b
Max Mason <maxdoylemason@gmail.com>
2022-11-01 15:11:06 +0000

    added mountain and beach pictures

--- src/pages/Beach.js
+++ src/pages/Beach.js
@@ -1,5 +1,13 @@
 const Beach = () => {
-    return <h1>Beach</h1>;
+    return (
+      <div>
+        <img src="https://thumbs.dreamstime.com/b/sunrise-over-beach-cancun-beautiful-mexico-40065727.jpg"></img>
+        
+        <div className="footer">
+          <a href="https://github.com/maxmason0322/creative-project-3">Github Repository</a>
+        </div>
+      </div>
+    );
   };
   
   export default Beach;
--- src/pages/Mountain.js
+++ src/pages/Mountain.js
@@ -1,5 +1,13 @@
 const Mountain = () => {
-    return <h1>Mountain</h1>;
+    return (
+      <div>
+        <img src="https://images.immediate.co.uk/production/volatile/sites/4/2021/08/mountains-7ddde89.jpg?quality=90&webp=true&resize=556,236"></img>
+      
+        <div className="footer">
+          <a href="https://github.com/maxmason0322/creative-project-3">Github Repository</a>
+        </div>
+      </div>
+    );
   };
   
   export default Mountain;
